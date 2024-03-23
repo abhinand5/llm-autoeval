f36492433475fa3ad6912edcbac7c3bbc204ed27
Abhinand <abhinandb.ml@gmail.com>
2024-03-23 11:10:59 +0530

    Remove gpu_mem util arg in open llm commands

--- runpod.sh
+++ runpod.sh
@@ -89,7 +89,7 @@ elif [ "$BENCHMARK" == "openllm" ]; then
 
     benchmark="arc"
     python main.py --model hf-causal-experimental \
-        --model_args pretrained=${MODEL_ID},dtype=auto,gpu_memory_utilization=0.8,trust_remote_code=$TRUST_REMOTE_CODE,revision=$revision \
+        --model_args pretrained=${MODEL_ID},dtype=auto,trust_remote_code=$TRUST_REMOTE_CODE,revision=$revision \
         --tasks arc_challenge \
         --num_fewshot 25 \
         --batch_size auto \
@@ -97,7 +97,7 @@ elif [ "$BENCHMARK" == "openllm" ]; then
 
     benchmark="hellaswag"
     python main.py --model hf-causal-experimental \
-        --model_args pretrained=${MODEL_ID},dtype=auto,gpu_memory_utilization=0.8,trust_remote_code=$TRUST_REMOTE_CODE,revision=$revision \
+        --model_args pretrained=${MODEL_ID},dtype=auto,trust_remote_code=$TRUST_REMOTE_CODE,revision=$revision \
         --tasks hellaswag \
         --num_fewshot 10 \
         --batch_size auto \
@@ -105,7 +105,7 @@ elif [ "$BENCHMARK" == "openllm" ]; then
 
     benchmark="mmlu"
     python main.py --model hf-causal-experimental \
-        --model_args pretrained=${MODEL_ID},dtype=auto,gpu_memory_utilization=0.8,trust_remote_code=$TRUST_REMOTE_CODE \
+        --model_args pretrained=${MODEL_ID},dtype=auto,trust_remote_code=$TRUST_REMOTE_CODE \
         --tasks hendrycksTest-abstract_algebra,hendrycksTest-anatomy,hendrycksTest-astronomy,hendrycksTest-business_ethics,hendrycksTest-clinical_knowledge,hendrycksTest-college_biology,hendrycksTest-college_chemistry,hendrycksTest-college_computer_science,hendrycksTest-college_mathematics,hendrycksTest-college_medicine,hendrycksTest-college_physics,hendrycksTest-computer_security,hendrycksTest-conceptual_physics,hendrycksTest-econometrics,hendrycksTest-electrical_engineering,hendrycksTest-elementary_mathematics,hendrycksTest-formal_logic,hendrycksTest-global_facts,hendrycksTest-high_school_biology,hendrycksTest-high_school_chemistry,hendrycksTest-high_school_computer_science,hendrycksTest-high_school_european_history,hendrycksTest-high_school_geography,hendrycksTest-high_school_government_and_politics,hendrycksTest-high_school_macroeconomics,hendrycksTest-high_school_mathematics,hendrycksTest-high_school_microeconomics,hendrycksTest-high_school_physics,hendrycksTest-high_school_psychology,hendrycksTest-high_school_statistics,hendrycksTest-high_school_us_history,hendrycksTest-high_school_world_history,hendrycksTest-human_aging,hendrycksTest-human_sexuality,hendrycksTest-international_law,hendrycksTest-jurisprudence,hendrycksTest-logical_fallacies,hendrycksTest-machine_learning,hendrycksTest-management,hendrycksTest-marketing,hendrycksTest-medical_genetics,hendrycksTest-miscellaneous,hendrycksTest-moral_disputes,hendrycksTest-moral_scenarios,hendrycksTest-nutrition,hendrycksTest-philosophy,hendrycksTest-prehistory,hendrycksTest-professional_accounting,hendrycksTest-professional_law,hendrycksTest-professional_medicine,hendrycksTest-professional_psychology,hendrycksTest-public_relations,hendrycksTest-security_studies,hendrycksTest-sociology,hendrycksTest-us_foreign_policy,hendrycksTest-virology,hendrycksTest-world_religions \
         --num_fewshot 5 \
         --batch_size auto \
@@ -113,7 +113,7 @@ elif [ "$BENCHMARK" == "openllm" ]; then
     
     benchmark="truthfulqa"
     python main.py --model hf-causal-experimental \
-        --model_args pretrained=${MODEL_ID},dtype=auto,gpu_memory_utilization=0.8,trust_remote_code=$TRUST_REMOTE_CODE,revision=$revision \
+        --model_args pretrained=${MODEL_ID},dtype=auto,trust_remote_code=$TRUST_REMOTE_CODE,revision=$revision \
         --tasks truthfulqa \
         --num_fewshot 0 \
         --batch_size auto \
@@ -121,7 +121,7 @@ elif [ "$BENCHMARK" == "openllm" ]; then
     
     benchmark="winogrande"
     python main.py --model hf-causal-experimental \
-        --model_args pretrained=${MODEL_ID},dtype=auto,gpu_memory_utilization=0.8,trust_remote_code=$TRUST_REMOTE_CODE,revision=$revision \
+        --model_args pretrained=${MODEL_ID},dtype=auto,trust_remote_code=$TRUST_REMOTE_CODE,revision=$revision \
         --tasks winogrande \
         --num_fewshot 5 \
         --batch_size auto \
@@ -129,7 +129,7 @@ elif [ "$BENCHMARK" == "openllm" ]; then
     
     benchmark="gsm8k"
     python main.py --model hf-causal-experimental \
-        --model_args pretrained=${MODEL_ID},dtype=auto,gpu_memory_utilization=0.8,trust_remote_code=$TRUST_REMOTE_CODE,revision=$revision \
+        --model_args pretrained=${MODEL_ID},dtype=auto,trust_remote_code=$TRUST_REMOTE_CODE,revision=$revision \
         --tasks gsm8k \
         --num_fewshot 5 \
         --batch_size auto \
